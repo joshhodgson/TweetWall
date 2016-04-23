d0e674df18769fcb3a63a190ae0870df3ae2cd5e
Josh Hodgson <josh.pbh@gmail.com>
2016-04-23 17:31:46 +0100

    Add regex character rejection and other stuff

--- index.js
+++ index.js
@@ -11,45 +11,52 @@ var io = require('socket.io')(server);
 
 var Twitter = require('node-tweet-stream');
 var tw = new Twitter({
-  consumer_key: config.twitter.consumer_key,
-  consumer_secret: config.twitter.consumer_secret,
-  token: config.twitter.token_key,
-  token_secret: config.twitter.token_secret
+    consumer_key: config.twitter.consumer_key,
+    consumer_secret: config.twitter.consumer_secret,
+    token: config.twitter.token_key,
+    token_secret: config.twitter.token_secret
 });
 
 
 for (var i in tracks) {
-  tw.track(tracks[i]);
+    tw.track(tracks[i]);
 }
 
 
-
+var timer = setTimeout(function() {}, 10);
+var tweetPending;
 
 tw.on('tweet', function(tweet) {
-  //console.log(JSON.stringify(tweet));
-  console.log("Tweet!");
-
-  var tweetText = "<a href='#'>" + tweet.user.name + "</a>: " + tweet.text;
-  tweetText = tweetText.replace(/[^A-Za-z 0-9 \.,\?""!@#\$%\^&\*\(\)-_=\+;:<>\/\\\|\}\{\[\]`~]*/g, '') ;
-  var team = Math.random() < 0.5 ? "lancs" : "york";
-  if (tweetText.toLowerCase().indexOf("lancaster") > -1) {
-    team = "lancs";
-  }
-  if (tweetText.toLowerCase().indexOf("york") > -1) {
-    team = "york";
-  }
-  if (tweetText.toLowerCase().indexOf("rosesarewhite") > -1) {
-    team = "york";
-  }
-  if (tweetText.toLowerCase().indexOf("rosesarered") > -1) {
-    team = "lancs";
-  }
+    //console.log(JSON.stringify(tweet));
+    var tweetText = tweet.user.name + ": " + tweet.text;
+    tweetText = tweetText.replace(/[^a-zA-Z0-9.,/#@+:\-_=;£$%&*!?()<> ]/g, '');
 
 
-  if (team) {
-    io.emit('tweet', {
-      team: team,
-      text: tweetText
-    });
-  }
+    var team = Math.random() < 0.5 ? "lancs" : "york";
+
+    if (tweetText.toLowerCase().indexOf("lancaster") > -1) {
+        team = "lancs";
+    }
+    if (tweetText.toLowerCase().indexOf("york") > -1) {
+        team = "york";
+    }
+    if (tweetText.toLowerCase().indexOf("rosesarewhite") > -1) {
+        team = "york";
+    }
+    if (tweetText.toLowerCase().indexOf("rosesarered") > -1) {
+        team = "lancs";
+    }
+
+    tweetPending = {
+        team: team,
+        text: tweetText
+    }
+    if (team) {
+        if (timer._called) {
+            timer = setTimeout(function() {
+                io.emit('tweet', tweetPending);
+                console.log(JSON.stringify(tweetPending.text))
+            }, 500)
+        }
+    }
 });
